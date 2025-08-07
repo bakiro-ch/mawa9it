c18e0a81eabf13c5e208631120d993d2c409a978
baki <bakihnma10101@gmail.com>
2025-08-07 22:40:43 +0100

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import { LocationContext } from "../contexts/Context";
 import { useEffect, useState } from "react";
 import { useSetIpApi } from "./features/location/hooks/useSetIpApi";
